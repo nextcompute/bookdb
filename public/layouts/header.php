fbb1b46fe96a44508116cfeed1b3cdbaf68b1602
Clinton Mendonca <clinton@nextcompute.com>
2015-08-30 17:15:28 +0400

    Create header.php in public/layouts folder.

---
+++ public/layouts/header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Book Keeping</title>
+    <link href="stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>Book Keeping</h1>
+    </div>
+    <div id="main">
