1b2f59479990b0056182d46b7c46723435f016c3
Clinton Mendonca <clinton@nextcompute.com>
2015-08-30 18:07:52 +0400

    Add Navigation Table to navigation.php.

--- public/layouts/navigation.php
+++ public/layouts/navigation.php
@@ -1,3 +1,13 @@
 <?php ?>
-<div id = "navigation"> <h2>Navigation</h2>  </div>
+<div id = "navigation"> <h2>Navigation</h2>  
+
+    <table>
+        <th>Page 1 </th>
+        <th>Page 2 </th>
+        <th>Page 3 </th>
+        <th>Page 4 </th>
+        <th>Page 5 </th>
+    </table>
+    
+</div>
 
