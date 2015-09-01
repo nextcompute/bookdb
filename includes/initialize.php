49f3566ed4a4f9f386d73aebdaa84ac0ea48a4f6
Clinton Mendonca <clinton@nextcompute.com>
2015-09-01 16:33:54 +0400

    Create initialize.php in includes folder.
    Definte DS as DIRECTORY_SEPARATOR.
    Define SITE_ROOT, LIB_PATH.
    
    Include config.php and functions.php.

---
+++ includes/initialize.php
@@ -0,0 +1,27 @@
+<?php
+
+// Define the core paths
+// Define them as absolute paths to make sure that require_once works as expected
+
+// DIRECTORY_SEPARATOR is a PHP pre-defined constant
+// (\ for Windows, / for Unix)
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+// D:\xampp\htdocs\bookdb
+defined('SITE_ROOT') ? null : 
+	define('SITE_ROOT', 'D:'.DS.'xampp'.DS.'htdocs'.DS.'bookdb');
+
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes');
+
+// load config file first
+require_once(LIB_PATH.DS.'config.php');
+
+// load basic functions next so that everything after can use them
+require_once(LIB_PATH.DS.'functions.php');
+
+// load core objects
+
+
+
+// load database-related classes
+
