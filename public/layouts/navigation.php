63333139794fdca0363730cfbf12ec3ff8517914
Clinton Mendonca <clinton@nextcompute.com>
2015-09-01 15:16:17 +0400

    Navigate via $pages() instead of page numbers.

--- public/layouts/navigation.php
+++ public/layouts/navigation.php
@@ -1,10 +1,17 @@
-<?php ?>
-<div id = "navigation"> <h2>Navigation</h2>  
+<?php 
+    $page_title = 'navigation';
+    if (isset($_GET['page'])){
+        $page_title = $_GET['page'];
+    } 
+?>
+
+<div id = "navigation"> <h2><?php echo ucfirst($page_title); ?></h2>  
 
     <table>
         <?php 
-        for ($i = 1; $i <= 5; $i++){
-            echo "<th><a href=\"index.php?page=$i\">Page $i</a></th>";
+        $pages = ['entries', 'accounts'];
+        for ($i = 0; $i < count($pages); $i++){
+            echo "<th><a href=\"index.php?page=$pages[$i]\">" . ucfirst($pages[$i]) . "</a></th>";
         }
         ?>
     </table>
