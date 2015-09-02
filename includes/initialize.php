9ce06abb5c642118c19ffe4d1793d471f77f8b70
Clinton Mendonca <clinton@nextcompute.com>
2015-09-02 17:08:47 +0400

    Require database_object.php and entry.php in initialize.php

--- includes/initialize.php
+++ includes/initialize.php
@@ -21,8 +21,10 @@ require_once(LIB_PATH.DS.'functions.php');
 
 // load core objects
 require_once(LIB_PATH.DS.'database.php');
+require_once(LIB_PATH.DS.'database_object.php');
 
 
 
 // load database-related classes
+require_once(LIB_PATH.DS.'entry.php');
 
