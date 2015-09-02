3af58cabcbe4dcf362c8de3925c4e606dbb57ef3
Clinton Mendonca <clinton@nextcompute.com>
2015-09-02 15:40:03 +0400

    Fix Database constants in config.php.

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "bookdb");
-defined('DB_PASS')   ? null : define("DB_PASS", "book");
-defined('DB_NAME')   ? null : define("DB_NAME", "Yv6sYfYJ2ciJ");
+defined('DB_USER')   ? null : define("DB_USER", "book");
+defined('DB_PASS')   ? null : define("DB_PASS", "Yv6sYfYJ2ciJ");
+defined('DB_NAME')   ? null : define("DB_NAME", "bookdb");
 
