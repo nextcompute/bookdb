b35e603b9f4e1c38990bc58fb25fc57727206c77
Clinton Mendonca <clinton@nextcompute.com>
2015-09-02 16:59:02 +0400

    Declare variables in Entry class.
    Set protectected static $table_name and $db_fields[];
    Set public $id,$amount,$debit_id,$credit_id,$created,$description.

--- includes/entry.php
+++ includes/entry.php
@@ -3,6 +3,16 @@
 require_once (LIB_PATH.DS.'database.php');
 
 class Entry extends DatabaseObject {
+   
+    protected static $table_name="entries";
+    protected static $db_fields = array('id', 'amount', 'debit_id', 'credit_id', 'created','description');
+    
+    public $id;
+    public $amount;
+    public $debit_id;
+    public $credit_id;
+    public $created;
+    public $description;
     
 }
 
