7cd9c35870b97728170479b537d342017e9604a4
Clinton Mendonca <clinton@nextcompute.com>
2015-09-03 14:39:59 +0400

    Create table_rows() in functions.php.
    Returns rows of values based from objects in array;

--- includes/functions.php
+++ includes/functions.php
@@ -20,3 +20,17 @@ function table_headings($fields){
     return $output;
 }
 
+function table_rows($result_array,$fields){
+    $output = "";
+    for ($i=0; $i<count($result_array); $i++){
+        $output .= "<tr>";
+        foreach ($fields as $field){
+            $output .= "<td>";
+            $output .= $result_array[$i]->$field;
+            $output .= "</td>";
+        }
+        $output .= "</tr>";
+    }
+    return $output;
+}
+
