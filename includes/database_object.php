48ab448b011501d239bc3455079fae1b98838613
Clinton Mendonca <clinton@nextcompute.com>
2015-09-03 13:47:57 +0400

    Use DatabaseObject class extension for Common Database Methods.
    Add methods to DatabaseObject class from Entry class:
    find_all(), find_by_sql(), instantiate(), has_attribute(), attributes().
    Use Late Static Bindings for use with child objects.
    Comment out above methods from Entry class.

--- includes/database_object.php
+++ includes/database_object.php
@@ -3,8 +3,49 @@
 require_once (LIB_PATH.DS.'database.php');
 
 class DatabaseObject {
-    
-    
+
+    public static function find_all() {
+        return self::find_by_sql("SELECT * FROM " . static::$table_name);
+    }
+
+    public static function find_by_sql($sql="") {
+    global $database;
+    $result_set = $database->query($sql);
+    $object_array = array();
+    while ($row = $database->fetch_array($result_set)) {
+      $object_array[] = self::instantiate($row);
+    }
+    return $object_array;
+  }
+
+    private static function instantiate($record) {
+    // Could check that $record exists and is an array
+        $object = new static;
+        // More dynamic, short-form approach:
+        foreach($record as $attribute=>$value){
+            if($object->has_attribute($attribute)) {
+                $object->$attribute = $value;
+            }
+        }
+        return $object;
+    }
+
+    private function has_attribute($attribute){
+    // We don't care about the value, we just want to know if the key exists
+    // Will return true or false
+        return array_key_exists($attribute, $this->attributes());
+    }
+
+    protected function attributes() {
+    // return an array of attribute names and their values
+        $attributes = array();
+        foreach(static::$db_fields as $field) {
+            if(property_exists($this, $field)) {
+                $attributes[$field] = $this->$field;
+            }
+        }
+        return $attributes;
+    }
     
 }
 
