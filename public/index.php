25d152ec0d8fefede39189bdf6032f5840003911
Clinton Mendonca <clinton@nextcompute.com>
2015-09-07 12:55:40 +0400

    Display tables from database based selector.
    Use navigation.php to set $class_name.
    Use $class_name for find_all() and table_fields().
    Display table using table_headings() and tables_rows().

--- public/index.php
+++ public/index.php
@@ -2,7 +2,31 @@
 require_once ('../includes/initialize.php');
 
 include('/layouts/header.php');
-include('/layouts/navigation.php');
+require_once('/layouts/navigation.php');
+//$page_tile is from navigation.php
+if ($page_title == 'entries') {
+    $class_name = 'Entry';
+} else if ($page_title == 'accounts_balances'){
+    $class_name = 'AccountBalance';
+}
+
+$object_array = "";
+$fields = "";
+
+if (isset($class_name)){
+
+    $object_array = $class_name::find_all();
+    $fields = $class_name::table_fields();
+}
+
+$output = "";
+$output .= "<table width  = 50%>";
+
+$output .= table_headings($fields);
+$output .= table_rows($object_array,$fields);
+
+$output .= "</table>";
+echo $output;
 
 include('/layouts/footer.php');
 
