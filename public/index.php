052ae658b6eb497f27107e6901d1983ed7f101f6
Clinton Mendonca <clinton@nextcompute.com>
2015-09-08 14:33:49 +0400

    Add Pagination to index.php.

--- public/index.php
+++ public/index.php
@@ -16,13 +16,60 @@ $object_array = "";
 $fields = "";
 $result_array = [];
 if (isset($class_name)){
+    
+    // 1. the current page number ($current_page)
+    $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
 
-    $object_array = $class_name::find_all();
+    // 2. records per page ($per_page)
+    $per_page = 10;
+    // 3. total record count ($total_count)
+    $total_count = $class_name::count_all();
+    
+    //$object_array = $class_name::find_all();
     $fields = $class_name::table_fields();
+    
+    $pagination = new Pagination($page, $per_page, $total_count);
+    $sql = "SELECT * FROM " . $class_name::table_name();
+    $sql .= " LIMIT {$per_page} ";
+    $sql .= " OFFSET {$pagination->offset()}";
+    $object_array = $class_name::find_by_sql($sql);
+    
+    
     for ($i=0; $i<count($object_array); $i++){
         $result_array[] = $object_array[$i]->object_to_assoc($fields);
     }
 }
+?>
+    <div id="pagination" style="clear: both;">
+    <?php  if(isset($pagination)){
+            if($pagination->total_pages() > 1) {
+                    if($pagination->has_previous_page()) { 
+            echo "<a href=\"index.php?view=$view_title&page=";
+          echo $pagination->previous_page();
+          echo "\">&laquo; Previous";
+          echo "</a> "; 
+        } else {
+          echo "&laquo; Previous";
+        }
+                    for($i=1; $i <= $pagination->total_pages(); $i++) {
+                            if($i == $page) {
+                                    echo " <span class=\"selected\">{$i}</span> ";
+                            } else {
+                                    echo " <a href=\"index.php?view=$view_title&page={$i}\">{$i}</a> "; 
+                            }
+                    }
+                    if($pagination->has_next_page()) { 
+                            echo " <a href=\"index.php?view=$view_title&page=";
+                            echo $pagination->next_page();
+                            echo "\">Next &raquo;</a> "; 
+                    } else {
+                        echo "Next &raquo;</a> "; 
+                    }
+            }
+    }
+    ?>
+    </div>
+<?php
 
 $output = "";
 $output .= "<table width  = 50%>";
