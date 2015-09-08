24eececd5cbbdbeb9f98e307b2c20bb3f4aea365
Clinton Mendonca <clinton@nextcompute.com>
2015-09-08 12:20:22 +0400

    Create constructor for Pagination class.

--- includes/pagination.php
+++ includes/pagination.php
@@ -6,4 +6,10 @@ class Pagination {
     public $current_page;
     public $per_page;
     public $total_count;
+    
+    public function __construct($page=1, $per_page=20, $total_count=0){
+  	$this->current_page = (int)$page;
+        $this->per_page = (int)$per_page;
+        $this->total_count = (int)$total_count;
+    }
 }
