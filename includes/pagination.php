097ce4fae7fcdce4ea929e2d95084b200b6d8f99
Clinton Mendonca <clinton@nextcompute.com>
2015-09-08 12:22:18 +0400

    Create other methods for Pagination class.

--- includes/pagination.php
+++ includes/pagination.php
@@ -12,4 +12,32 @@ class Pagination {
         $this->per_page = (int)$per_page;
         $this->total_count = (int)$total_count;
     }
+    
+    public function offset() {
+        // Assuming 20 items per page:
+        // page 1 has an offset of 0    (1-1) * 20
+        // page 2 has an offset of 20   (2-1) * 20
+        //   in other words, page 2 starts with item 21
+        return ($this->current_page - 1) * $this->per_page;
+    }
+
+    public function total_pages() {
+        return ceil($this->total_count/$this->per_page);
+    }
+	
+    public function previous_page() {
+        return $this->current_page - 1;
+    }
+  
+    public function next_page() {
+        return $this->current_page + 1;
+    }
+
+    public function has_previous_page() {
+        return $this->previous_page() >= 1 ? true : false;
+    }
+
+    public function has_next_page() {
+        return $this->next_page() <= $this->total_pages() ? true : false;
+    }
 }
