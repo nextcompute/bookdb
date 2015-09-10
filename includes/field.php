199321644f6e1ff94f6e7a841f2534add4f61563
Clinton Mendonca <clinton@nextcompute.com>
2015-09-10 15:32:47 +0400

    Declare static $db_fields[] for Field class.
    show_fields() overrides DatabaseObject and returns NULL.

--- includes/field.php
+++ includes/field.php
@@ -2,5 +2,14 @@
 
 class Field extends DatabaseObject {
     
+    protected static $db_fields = array('Field', 'Type', 'Key');
+    
+    public $Field;
+    public $Type;
+    public $Key;
+    
+    protected static function show_fields(){
+        return NULL;
+    }
 }
 
