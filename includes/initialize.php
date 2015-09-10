0908ba1bd67dc43a7aab8ef80e933aa8a8ab3e33
Clinton Mendonca <clinton@nextcompute.com>
2015-09-10 15:33:37 +0400

    Require field.php in initiliaze.php.

--- includes/initialize.php
+++ includes/initialize.php
@@ -23,6 +23,7 @@ require_once(LIB_PATH.DS.'functions.php');
 require_once(LIB_PATH.DS.'database.php');
 require_once(LIB_PATH.DS.'database_object.php');
 require_once(LIB_PATH.DS.'pagination.php');
+require_once(LIB_PATH.DS.'field.php');
 
 
 
