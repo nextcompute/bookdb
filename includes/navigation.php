c3a0c0c9d2303b1bfc95e97fe7d7735982bf978f
Clinton Mendonca <clinton@nextcompute.com>
2015-09-15 12:15:01 +0400

    Declare database variables for Navigation class.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,4 +1,10 @@
 <?php
 class Navigation extends DatabaseObject {
-       
+    
+    protected static $table_name = "navigation";
+    protected static $db_fields = array('id', 'name' ,'location',);
+    
+    public $id;
+    public $name;
+    public $location;
 }
