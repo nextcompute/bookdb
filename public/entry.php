04ffcbfa20c34f1dfb706ffea08bacecfc9d22ee
Clinton Mendonca <clinton@nextcompute.com>
2015-09-20 15:49:07 +0400

    Create entry.php in public folder.

---
+++ public/entry.php
@@ -0,0 +1,9 @@
+<?php
+require_once ('../includes/initialize.php');
+
+include('/layouts/header.php');
+$page_name = "entry";
+require_once('/layouts/navigation.php');
+include('/layouts/footer.php');
+
+
