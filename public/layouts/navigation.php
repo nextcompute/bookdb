87b02df995e66b95107b0e3808989b360e4b7f53
Clinton Mendonca <clinton@nextcompute.com>
2015-09-20 15:47:53 +0400

    Use Navigation class to display links to various pages.

--- public/layouts/navigation.php
+++ public/layouts/navigation.php
@@ -1,21 +1,18 @@
 <?php
-    include_once '../includes/functions.php';
-    $view_title = 'home';
-    if (isset($_GET['view'])){
-        $view_title = $_GET['view'];
-        if ($view_title == 'home'){
-            redirect_to('.');
-        }
-    } 
+    include_once (LIB_PATH.DS.'functions.php');
 ?>
 
-<div id = "navigation"> <h2><?php echo format_field_name($view_title); ?></h2>  
-
+<div id = "navigation"> <h2><?php echo format_field_name($page_name); ?></h2>  
+    
     <table>
         <?php 
-        $views = ['home', 'entries', 'accounts', 'accounts_balances'];
-        for ($i = 0; $i < count($views); $i++){
-            echo "<th><a href=\"index.php?view=" . htmlentities($views[$i]) . "\">" . format_field_name($views[$i]) . "</a></th>";
+        //$views = ['home', 'entries', 'accounts', 'accounts_balances'];
+        $views = Navigation::find_all();
+        foreach ($views as $view) {
+            if($location = $view->location){
+                $location .= ".php";
+            }
+            echo "<th><a href=\"./" . htmlentities($location) . "\">" . format_field_name($view->name) . "</a></th>";
         }
         ?>
     </table>
