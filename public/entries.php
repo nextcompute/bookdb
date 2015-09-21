185f6f52a1f8ed5e76776f5db855d0ccab831e60
Clinton Mendonca <clinton@nextcompute.com>
2015-09-21 12:24:52 +0400

    Use Pagination class in entries.php for display_table().

--- public/entries.php
+++ public/entries.php
@@ -3,15 +3,61 @@ require_once ('../includes/initialize.php');
 
 include('/layouts/header.php');
 $page_name = "entry";
-require_once('/layouts/navigation.php');
+// 1. the current page number ($current_page)
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
 
-$object_array = Entry::find_all();
-$fields = Entry::table_fields();    
+// 2. records per page ($per_page)
+$per_page = 10;
+// 3. total record count ($total_count)
+$total_count = Entry::count_all();
+
+//$object_array = Entry::find_all();
+$fields = Entry::table_fields();
+
+$pagination = new Pagination($page, $per_page, $total_count);
+$sql = "SELECT * FROM " . Entry::table_name();
+$sql .= Entry::sql_order();
+$sql .= " LIMIT {$per_page} ";
+$sql .= " OFFSET {$pagination->offset()}";
+$object_array = Entry::find_by_sql($sql);
 
 for ($i=0; $i<count($object_array); $i++){
     $result_array[] = $object_array[$i]->object_to_assoc($fields);
 }
 
+require_once('/layouts/navigation.php');
+?>
+<div id="pagination" style="clear: both;">
+<?php  
+if(isset($pagination)){
+    if($pagination->total_pages() > 1) {
+        if($pagination->has_previous_page()) { 
+            echo "<a href=\"?page=";
+            echo $pagination->previous_page();
+            echo "\">&laquo; Previous";
+            echo "</a> "; 
+        } else {
+            echo "&laquo; Previous";
+        }
+        for($i=1; $i <= $pagination->total_pages(); $i++) {
+            if($i == $page) {
+                echo " <span class=\"selected\">{$i}</span> ";
+            } else {
+                echo " <a href=\"?page={$i}\">{$i}</a> "; 
+            }
+        }
+        if($pagination->has_next_page()) { 
+            echo " <a href=\"?page=";
+            echo $pagination->next_page();
+            echo "\">Next &raquo;</a> "; 
+        } else {
+            echo "Next &raquo;</a> "; 
+        }
+    }
+}
+?>
+</div>
+<?php
 echo display_table($result_array,$fields);
 include('/layouts/footer.php');
 
