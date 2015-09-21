442af36088fa2c24f61e0d9e223be02d6402f6cd
Clinton Mendonca <clinton@nextcompute.com>
2015-09-21 11:52:02 +0400

    Display all Entries in entry.php.

--- public/entry.php
+++ public/entry.php
@@ -4,6 +4,15 @@ require_once ('../includes/initialize.php');
 include('/layouts/header.php');
 $page_name = "entry";
 require_once('/layouts/navigation.php');
+
+$object_array = Entry::find_all();
+$fields = Entry::table_fields();    
+
+for ($i=0; $i<count($object_array); $i++){
+    $result_array[] = $object_array[$i]->object_to_assoc($fields);
+}
+
+echo display_table($result_array,$fields);
 include('/layouts/footer.php');
 
 
